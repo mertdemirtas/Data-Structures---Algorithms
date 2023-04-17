e14cf487b7fe822f023946b2e0bb816b17a3c040
mertdemirtas <m.demirtas99@icloud.com>
2023-04-17 22:24:20 +0300

    layout fix

--- Python3/Leetcode/main.py
+++ Python3/Leetcode/main.py
@@ -1,5 +1,28 @@
 # MARK: Dynamic Programming
+class NthTribonacciNumber:
+    def __init__(self):
+        n = 25
+        print(self.__tribonacci(n))
+
+    def __tribonacci(self, n: int) -> int:
+        return self.__tribonacci_helper(n)
+
+    def __tribonacci_helper(self, n: int, memo = {}):
+        if n in memo: return memo[n]
+        if n == 0: return 0
+        if n <= 2: return 1
+
+        memo[n] = self.__tribonacci_helper(n - 1) + self.__tribonacci_helper(n - 2) + self.__tribonacci_helper(n - 3)
+
+        return memo[n]
+
+    def run(self, n: int):
+        self.__tribonacci(n)
 class MinCostClimbingStairs:
+    def __init__(self):
+        arr = [1,100,1,1,1,100,1,1,100,1]
+        print("Min cost to reach the top of stairs:", self.__min_cost_climbing_stairs(arr))
+
     def __min_cost_climbing_stairs(self, cost: []) -> int:
         return min(self.__min_cost_climbing_stairs_helper(cost, len(cost) - 1), self.__min_cost_climbing_stairs_helper(cost, len(cost) - 2))
 
@@ -13,6 +36,10 @@ class MinCostClimbingStairs:
     def run(self, arr: []):
         print("Min cost to reach the top of stairs:", self.__min_cost_climbing_stairs(arr))
 class HouseRobber:
+    def __init__(self):
+        arr = [2,7,9,3,1]
+        print("Max profit for house robbing:", self.__rob(arr))
+
     def __rob(self, nums: []) -> int:
         return max(self.__rob_helper(nums, len(nums) - 1), self.__rob_helper(nums, len(nums) - 2))
 
@@ -24,15 +51,12 @@ class HouseRobber:
         return memo[n]
 
     def run(self, arr: []):
-        print("Max profit for house robbing:", self.__rob(nums))
+        print("Max profit for house robbing:", self.__rob(arr))
 
+nth_tribonacci_number = NthTribonacciNumber()
 climbing_stairs = MinCostClimbingStairs()
-cost = [1,100,1,1,1,100,1,1,100,1]
-climbing_stairs.run(cost)
-
 house_robber = HouseRobber()
-nums = [2,7,9,3,1]
-house_robber.run(nums)
+
 
 # ---------------------------------------------------------------------------------
 
